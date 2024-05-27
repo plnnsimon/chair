c2031e8a4c533be65640385d36a3dbbf5981e9fd
Олександр Пальонний <s2567k@gmail.com>
2024-05-28 00:51:12 +0300

    eee

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,21 @@ import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/chair/',
 
   // envDir,
 
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 3002,
-  //   fs: {
-  //     strict: false
-  //   }
-  // },
+  server: {
+    // host: '0.0.0.0',
+    // port: 3002,
+    fs: {
+      strict: false
+    }
+  },
 
   build: {
     sourcemap: true,
-    
+
     rollupOptions: {
       input: 'index.html'
     }
