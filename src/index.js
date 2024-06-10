839720f311283d7a9073a662d2d74e5e8a0865b1
Олександр Пальонний <s2567k@gmail.com>
2024-06-10 13:35:08 +0300

    thsh

--- src/index.js
+++ src/index.js
@@ -29,27 +29,30 @@ configuratorApplication.mount();
 // range.addEventListener('input', (ev) => {
 //   configuratorApplication.setIntensity(ev.target.value, 'fabric')
 // })
-  color.addEventListener('input', (ev) => {
-    configuratorApplication.setColor(ev.target.value, 'fabric');
-  });
+color.addEventListener('input', (ev) => {
+  configuratorApplication.setColor(ev.target.value, 'fabric');
+});
 
-  colorWood.addEventListener('input', (ev) => {
-    configuratorApplication.setColor(ev.target.value, 'wood');
-  });
+colorWood.addEventListener('input', (ev) => {
+  configuratorApplication.setColor(ev.target.value, 'wood');
+});
 
 configuratorApplication.initGLTFLoader('assets/models/SinteponС2.glb');
 
 const arr = [
+  'rgb(61, 112, 179)',
   'rgb(127, 94, 72)',
   'rgb(77, 77, 77)',
   'rgb(52, 126, 42)',
   'rgb(184, 184, 184)',
   'rgb(172, 12, 12)',
-  'rgb(255, 97, 160)',
-  'rgb(61, 112, 179)'
+  'rgb(255, 97, 160)'
 ];
 
+const arrWoods = ['rgb(246, 147, 55)', 'rgb(114, 59, 8)'];
+
 const colors = document.getElementById('colors');
+const woods = document.getElementById('woods');
 
 arr.forEach((c) => {
   const div = document.createElement('div');
@@ -60,3 +63,20 @@ arr.forEach((c) => {
   });
   colors.appendChild(div);
 });
+
+arrWoods.forEach((c) => {
+  const div = document.createElement('div');
+  div.style.backgroundColor = c;
+  div.className = 'color-item';
+  div.addEventListener('click', (ev) => {
+    configuratorApplication.setColor(ev.target.style.backgroundColor, 'wood');
+  });
+  woods.appendChild(div);
+});
+
+
+emitter.subscribe('setInitFinished', (ev) => {
+
+  configuratorApplication.setColor(arr[0], 'fabric')
+  configuratorApplication.setColor(arrWoods[0], 'wood')
+})
